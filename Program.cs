f16d582f00055c2e4e54c69b169619c469ff9875
ProgramerPlus <hipitelo@outlook.com>
2025-11-14 14:01:33 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("--- Estacionamento ---");
+Console.WriteLine("Tamanho do veículo (P/G).....: ");
+string tamanho=Console.ReadLine();
+Console.WriteLine("Tempo de permanência (min)...: ");
+int tempo=Convert.Int32(Console.ReadLine());
+Console.WriteLine("Serviço de vallet (s/n)......: ");
+string vallet=Console.ReadLine();
+Console.WriteLine("Serviço de lavagem (s/n).....:");
+string lavagem=Console.ReadLine();
+
+bool lavagem = lavagem == "s";
+bool vallet = vallet =="s";
+
+if()
